ba0b493c5a804676f18b7870b98c540b0f843b96
Alex <pichuev.alex@yandex.ru>
2021-02-27 12:49:47 +0700

    Добавьте файлы проекта.

---
+++ Converter/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Converter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void GiveResult(object sender, RoutedEventArgs e)
+        {
+            string text = FromValue.Text;
+            int fromNum = 0;
+            if (text != "")
+            {
+                try
+                {
+                    fromNum = Int32.Parse(FromValue.Text);
+                    ToValue.Text = $"Число: {fromNum}";
+                }
+                catch
+                {
+                    MessageBox.Show("Введено некорректное число");
+                }
+            }
+        }
+    }
+}
